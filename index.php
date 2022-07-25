ecf63e264572247a699f1110a419a500986c25fe
Handsome <rikhotsohandsome@gmail.com>
2022-07-25 20:30:58 +0200

    main page completion

--- index.php
+++ index.php
@@ -8,22 +8,85 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Check</title>
+	<title>b-School</title>
 </head>
 <body>
 	<nav>
-		<label class="logo">School</label>
+		<label class="logo">b-School</label>
 		<ul>
 			<li><a href="">Home</a></li>
 			<li><a href="">Contact</a></li>
 			<li><a href="">Admission</a></li>
 		 	<li><a class="btn btn-success" href="">Login</a></li>
-
 		</ul>
 	</nav>
 	<div class="section1">
-		<label class="img_text">Careful Coding.</label>
+		<label class="img_text">School Registration.</label>
 		<img class="main_img" src="/img/jj.jpg">
 	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="first_img" src="/img/d.jpg">
+			</div>
+				<div class="col-md-8">
+					<h1>Welcome to b-school</h1>
+					<p>dsfdf gsdg</p>		
+				</div>			
+		</div>
+	</div>
+
+
+	<center><h1>Our Teachers</h1></center>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="first_img" src="/img/c.jpg">
+			</div>
+				<div class="col-md-8">
+					<h1>Teacher1</h1>
+					<p>dsfdf gsdg</p>		
+				</div>			
+		</div>
+	</div>
+
+<center><h1 class="adm">Admission Form</h1></center>
+<div align="center" class="admission_form">
+		<form>
+			<div class="adm_int">
+				<label class="label_text">Name</label>
+				<input class="input_deg" type="text" name="">
+			</div>
+
+			<div class="adm_int">
+				<label class="label_text">Email</label>
+				<input class="input_deg" type="text" name="">
+			</div>
+			<div class="adm_int">
+				<label class="label_text">Phone</label>
+				<input class="input_deg" type="text" name="">
+			</div>
+			<div class="adm_int">
+				<label class="label_text">Message</label>
+				<textarea class="txtArea"></textarea>
+			</div>
+			<div>
+				<input class="btn btn-primary" value="Apply" id="submit" type="submit" >
+			</div>														
+		</form>
+</div>
+
+<footer>
+	<h2 class="footer_txt">All @copyright reserved by web tech knowledge.</h2>
+</footer>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
